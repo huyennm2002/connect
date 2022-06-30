b0f07958ac7f43a5e1a72de33a163bc56289e7dc
Huyen <huyennm2002@gmail.com>
2022-06-30 22:50:38 +0700

    updates for heroku deployment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import "controllers"
+import "./controllers"
 
 //= require jquery
 //= require jquery_ujs
