e91d345302d0140eedf624d0f6ccca4b570630e1
Huyen <huyennm2002@gmail.com>
2022-07-01 14:30:07 +0700

    add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+    :password => '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', # This is the secret sendgrid API key which was issued during API key creation
+    :domain => '@gmail.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'huyennm1104@gmail.com',
-    :password             => 'trjjmfacqnfdgnoc',
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => 'huyennm1104@gmail.com',
+  #   :password             => 'trjjmfacqnfdgnoc',
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
 end
