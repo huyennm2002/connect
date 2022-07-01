51109cd2f0ce82a1be40e4463c8156ef132fad8c
Huyen <huyennm2002@gmail.com>
2022-07-01 14:09:20 +0700

    config for fb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
 
-  SMTP settings for gmail
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
